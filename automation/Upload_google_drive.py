7698ac4b10791d1721c810dee27198b374e01de5
HynJngKng <39401060+HynJngKng@users.noreply.github.com>
2018-09-20 10:24:03 +0900

    Update Upload_google_drive.py

--- automation/Upload_google_drive.py
+++ automation/Upload_google_drive.py
@@ -5,33 +5,40 @@ from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 
-try :
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
 
-SCOPES = 'https://www.googleapis.com/auth/drive.file'
-store = file.Storage('storage.json')
-creds = store.get()
+def upload_google_api(filename, folder_id):
+    try :
+        import argparse
+        flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+    except ImportError:
+        flags = None
 
-if not creds or creds.invalid:
-    print("make new storage data file ")
-    flow = client.flow_from_clientsecrets('client_secret_drive.json', SCOPES)
-    creds = tools.run_flow(flow, store, flags) \
-            if flags else tools.run(flow, store)
+    SCOPES = 'https://www.googleapis.com/auth/drive.file'
+    store = file.Storage('storage.json')
+    creds = store.get()
 
-DRIVE = build('drive', 'v3', http=creds.authorize(Http()))
+    if not creds or creds.invalid:
+        print("make new storage data file ")
+        flow = client.flow_from_clientsecrets('client_secret_drive.json', SCOPES)
+        creds = tools.run_flow(flow, store, flags) \
+                if flags else tools.run(flow, store)
 
-FILES = (('hello.txt'),)
-folder_id = '1DDMvvmZmYhXyvX2mBZ5F2SE2t5IJr3DE'  # 폴더코드
+    DRIVE = build('drive', 'v3', http=creds.authorize(Http()))
 
-for file_title in FILES :
-    file_name = file_title
-    metadata = {'name': file_name, 'parents' : [folder_id],
-                'mimeType': None
-                }
+    FILES = (('hello.txt'),)
+    folder_id = '1DDMvvmZmYhXyvX2mBZ5F2SE2t5IJr3DE'  # 폴더코드
 
-    res = DRIVE.files().create(body=metadata, media_body=file_name).execute()
-    if res:
-        print('Uploaded "%s" (%s)' % (file_name, res['mimeType']))
+    for file_title in FILES :
+        file_name = file_title
+        metadata = {'name': file_name, 'parents' : [folder_id],
+                    'mimeType': None
+                    }
+        media = str("data\\" + file_name)
+        print(media)
+        res = DRIVE.files().create(body=metadata, media_body=media).execute()
+        if res:
+            print('Uploaded "%s" (%s)' % (file_name, res['mimeType']))
+
+
+if __name__ == '__main__':
+    upload_google_api('hello.txt', '1DDMvvmZmYhXyvX2mBZ5F2SE2t5IJr3DE')
